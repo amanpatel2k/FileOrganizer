46ff840fe48efeaff66acc619ebb10a52e317d52
Aman <amanpatel2k@gmail.com>
2023-09-09 15:32:13 -0700

    Implemented Search by Folder

--- template/file_searching.py
+++ template/file_searching.py
@@ -32,7 +32,7 @@ class File_Search:
                 return f'File Not Found by This Name: {file_name}'
         
         else:   
-            # Search by File Name and Extension
+            # Search by Both
             file_name = input("Enter the file you are finding: ")
             ext = input("Enter the extension of the file: ")
             full_file = file_name+ext
@@ -42,7 +42,14 @@ class File_Search:
             else: return f'File Not Found by This Name: {file_name}'
         
     def folder(self): 
-        pass
+        user_folder = input("What is the folder name: ")
+        directory = []
+        for root, dirs, files in os.walk(self.path): 
+            for dir in dirs: 
+                if dir.lower() == user_folder.lower(): 
+                    directory.append(os.path.join(root, dir))    
+        if directory: return directory
+        else: return f'Folder, {user_folder}, Not Found'
         
 def file_folder_search(): 
     
@@ -59,5 +66,5 @@ def file_folder_search():
         user = input("Would like to search for a file or for a folder: ")
     else: print('Invalid Input Please Try Again\n')  
 
-if __name__ == '__main__':
-    file_folder_search()
+# if __name__ == '__main__':
+#     file_folder_search()
